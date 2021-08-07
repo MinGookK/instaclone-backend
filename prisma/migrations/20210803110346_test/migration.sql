97d18725a12c123407fb583e1c398ea2b2fe3542
MinGook <mgk8609@naver.com>
2021-08-07 21:27:04 +0900

    다 지운 상태 / 이제 Users 모듈 만들러 가자~

--- prisma/migrations/20210803110346_test/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "published" BOOLEAN NOT NULL DEFAULT true,
-    "authorId" INTEGER NOT NULL,
-
-    PRIMARY KEY ("id")
-);
